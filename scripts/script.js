642d36a737479a61799902e5b7f51ee190b9d160
Amir <amirkadenov@gmail.com>
2019-11-03 19:44:49 +0600

    Fixed pricing modal js and added authorization with modal

--- scripts/script.js
+++ scripts/script.js
@@ -1,10 +1,27 @@
+
 $(document).ready(function () {
+
+
+$('#pricing-modal-toggle').click(function () {
     $('#pricingModal').modal({
         keyboard:false,
         backdrop:"static",
     });
 
+});
+
+    $('#auth-modal-toggle').click(function () {
+        $('#authModal').modal({
+            keyboard:false,
+            backdrop:"static",
+        });
+
+    });
+
+
 })
+
+
 
 
 
